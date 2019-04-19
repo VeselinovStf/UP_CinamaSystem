bb93232cf5dae9f1037a1a653bd3170b3f770f7e
VeselinovStf <veselinovStf@gmail.com>
2019-04-19 20:39:07 +0300

    Formating code

--- src/CinemAPI.Data.EF/Migrations/201904180407466_Adding AvailibleSeatsCount to Projjection.cs
+++ src/CinemAPI.Data.EF/Migrations/201904180407466_Adding AvailibleSeatsCount to Projjection.cs
@@ -1,15 +1,14 @@
 namespace CinemAPI.Data.EF
 {
-    using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class AddingAvailibleSeatsCounttoProjjection : DbMigration
     {
         public override void Up()
         {
             AddColumn("dbo.Projections", "AvailableSeatsCount", c => c.Int(nullable: false));
         }
-        
+
         public override void Down()
         {
             DropColumn("dbo.Projections", "AvailableSeatsCount");
